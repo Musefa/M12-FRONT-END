b49d69d6887038b445c9a12f6bf7290c224f9cc2
erga916 <erga916@vidalibarraquer.net>
2023-03-28 13:03:38 +0200

    LOGIN RULES

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import React from "react";
 import GruposList from "./grups/GruposList";
+import { LoginController } from './services/login';
 
 function App() {
   return (
     <div className="App">
       <h1>Lista de Grupos</h1>
       <GruposList />
+      <LoginController props = {{email : 'erga916@vidalibarraquer.net', password : 'Admin1234@'}} />
       {/* Otros componentes y contenido */}
     </div>
   );
