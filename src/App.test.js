f79ebf1ee57a4c2956c5573f3a209b49fb17c92f
jepa139 <jepa139@vidalibarraquer.net>
2023-04-26 09:49:54 +0200

    TESTING HEADER Y HOMEPAGE

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,38 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import HomePage from './pages/HomePage';
+import Header from './components/Header';
+import { UserProvider } from './contexts/UserContext';
+import { BrowserRouter } from 'react-router-dom'; // Importa BrowserRouter
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('HomePage', () => {
+  test('renders HomePage component', () => {
+    render(<HomePage />);
+
+    const title = screen.getByText(/Gestió de reunions/i);
+    expect(title).toBeInTheDocument();
+
+    const image = screen.getByAltText(/Home/i);
+    expect(image).toBeInTheDocument();
+    expect(image.src).toContain('kellogsFPD.jpg');
+  });
+});
+
+describe('Header', () => {
+  test('renders Header', () => {
+    render(
+      <UserProvider>
+        <BrowserRouter>
+          <Header />
+        </BrowserRouter>
+      </UserProvider>
+    );
+
+    const loginText = screen.getByText(/Iniciar sessió/i);
+    expect(loginText).toBeInTheDocument();
+
+    const image = screen.getByAltText(/Home/i);
+    expect(image).toBeInTheDocument();
+    expect(image.src).toContain('bannerVidal.png');
+  });
 });
